1ad86bf43270930c52ad1cd148823d354c03dabc
gambkler <gamb.kler@gmail.com>
2019-10-16 16:17:54 +0800

    test finished ?

--- README.md
+++ README.md
@@ -7,10 +7,12 @@
 ## article
 save original html and cleaned article
 1. `$x('//font[@size="2" and @face="verdana"]')`
+2. usually return two selector, use the first
 2. delete `<table>`
 3. treat `<br>` as newline, sometimes as paragraph separator (double br)
 4. delete `\n`?
 5. treat `<b>` as subtitle
-6. highlight `<a>`
-7. remove all `<element>`, expect elements: font, a, br, b
-8. html.escape, html.unescape
+6. treat `<hr>` as seperate line
+7. highlight `<a>`
+8. remove all `<element>`, expect elements: font, a, br, b
+9. html.escape, html.unescape
--- test.py
+++ test.py
@@ -5,53 +5,68 @@ class CustomException(Exception):
     pass
 
 class Cleaner:
-    def __init__(self, src_file='temp.html', dst_file='test.html'):
-        self._src_file_name = src_file
-        self._dst_file_name = dst_file
+    def __init__(self):
+        self._element_table_start_tag_pattern = re.compile(r'<table.*>')
+        self._element_table_end_tag_string = r'</table>'
+        self._raw_newline_pattern = re.compile(r'\\n')
+        self._element_br_pattern = re.compile(r'<\\?br>')
+        self._all_element_tag = re.compile(r'<.*?>')
+        self._blanks_pattern = re.compile(r' *\n *')
+        self._join_lines_pattern = re.compile(r'(\S)\n(\S)')
+        self._escaped_apostrophe_pattern = re.compile(r"\\'")
+        self._multi_emptyline_pattern = re.compile(r'\n{3,}')
 
-    def start(self):
-        self._src_file = open(self._src_file_name)
-        self._file_content = self._src_file.read()
-        self._src_file.close()
-        
+    def clean_from_file(self, src_file='temp.html', dst_file='test.html'):
+        with open(src_file) as f:
+            self._content = f.read()
+        self._process()
+        with open(dst_file, 'w') as f:
+            f.write(self._content)
+
+    def clean(self, content):
+        self._content = content
+        self._process()
+        return self._content
+
+    def _process(self):
         self._remove_element_table()
-        self._remove_newline()
+        self._remove_raw_newline()
         self._replace_element_br()
         self._remove_all_element_tag()
         self._remove_blanks()
         self._join_lines()
         self._unescape()
-
-        self._dst_file = open(self._dst_file_name, 'w')
-        self._dst_file.write(self._file_content)
-        self._dst_file.close()
+        self._remove_multi_emptyline()
 
     def _remove_element_table(self):
-        for i in re.finditer(r'<table.*>', self._file_content):
+        for i in self._element_table_start_tag_pattern.finditer(self._content):
             start_index = i.start()
-            end_index = self._file_content.find(r'</table>') + 8
-            self._file_content = self._file_content[:start_index] + self._file_content[end_index:]
+            end_index = self._content.find(self._element_table_end_tag_string) + 8
+            self._content = self._content[:start_index] + self._content[end_index:]
 
-    def _remove_newline(self):
-        self._file_content = re.sub(r'\\n', ' ', self._file_content)
+    def _remove_raw_newline(self):
+        self._content = self._raw_newline_pattern.sub(' ', self._content)
 
     def _replace_element_br(self):
-        self._file_content = re.sub(r'<\\?br>', '\n', self._file_content)
+        self._content = self._element_br_pattern.sub('\n', self._content)
     
     def _remove_all_element_tag(self):
-        self._file_content = re.sub(r'<.*?>', '', self._file_content)
+        self._content = self._all_element_tag.sub('', self._content)
     
     def _remove_blanks(self):
-        self._file_content = re.sub(r' *\n *', '\n', self._file_content)
+        self._content = self._blanks_pattern.sub('\n', self._content)
     
     def _join_lines(self):
         # twice to join '[1]' mark
-        self._file_content = re.sub(r'(\S)\n(\S)', r'\1 \2', self._file_content)
-        self._file_content = re.sub(r'(\S)\n(\S)', r'\1 \2', self._file_content)
+        self._content = self._join_lines_pattern.sub(r'\1 \2', self._content)
+        self._content = self._join_lines_pattern.sub(r'\1 \2', self._content)
     
     def _unescape(self):
-        self._file_content = html.unescape(self._file_content)
-        self._file_content = re.sub(r"\\'", "'", self._file_content)
+        self._content = html.unescape(self._content)
+        self._content = self._escaped_apostrophe_pattern.sub("'", self._content)
+    
+    def _remove_multi_emptyline(self):
+        self._content = self._multi_emptyline_pattern.sub('\n\n', self._content)
 
 c = Cleaner()
-c.start()
+c.clean_from_file()
